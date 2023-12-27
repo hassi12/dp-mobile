f84289c05453c98fed8b05ef70d429912a8cbaf9
Zaheer Jan <zaheer@cloaked.id>
2023-12-27 17:24:41 +0500

    listing api in home page

---
+++ services/Products_services.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export async function getProducts(){
+    let response = await axios.get(`http://ec2-43-206-254-199.ap-northeast-1.compute.amazonaws.com/api/v1/items/`)
+    return response.data
+}
