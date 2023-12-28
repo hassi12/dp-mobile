f75a636872fa0260a9bb346e0ce39a7162ae804c
Atif Badini <badiniatif@gmail.com>
2023-12-28 12:26:48 +0500

    new

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -79,7 +79,7 @@ const HomePage = () => {
   const ProductCategoryList = async () =>{
     try {
       let res = await axios.get(`http://ec2-43-206-254-199.ap-northeast-1.compute.amazonaws.com/api/v1/category/`)
-      console.log('cate',res.data.results)
+      console.log('categories',res.data.results)
       setCategory(res.data.results)
     } catch (error) {
       console.log(error);
