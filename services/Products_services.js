3bf02c49d420c108dbe58a009525a39d29aac9a4
Zaheer Jan <zaheer@cloaked.id>
2024-05-29 13:03:28 +0500

    new

--- services/Products_services.js
+++ services/Products_services.js
@@ -12,16 +12,16 @@ export async function searchProducts(headers,search){
 }
 
 export async function getProductDetail(productId){
-    let response = await axios.get(`http://ec2-43-206-254-199.ap-northeast-1.compute.amazonaws.com/api/v1/items/${productId}/detail_item/`)
+    let response = await axios.get(`${BASE_URL}api/v1/items/${productId}/detail_item/`)
     return response.data
 }
 
 export async function ProductsCategory(){
-    let response = await axios.get(`http://ec2-43-206-254-199.ap-northeast-1.compute.amazonaws.com/api/v1/category/`)
+    let response = await axios.get(`${BASE_URL}api/v1/category/`)
     return response.data
 }
 
 export async function FavouriteItems(headers){
-    let response = await axios.get('http://ec2-43-206-254-199.ap-northeast-1.compute.amazonaws.com/api/v1/favourite/items/', {headers:headers})
+    let response = await axios.get('${BASE_URL}api/v1/favourite/items/', {headers:headers})
     return response.data
 }
--- services/order_services.js
+++ services/order_services.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { BASE_URL } from './base_url'
 
 export async function Userorders(headers) {
   let response = await axios.get(
-    `http://ec2-43-206-254-199.ap-northeast-1.compute.amazonaws.com/api/v1/orders/`,
+    `${BASE_URL}api/v1/orders/`,
     {headers: headers},
   );
   return response.data;
@@ -10,14 +11,14 @@ export async function Userorders(headers) {
 
 export async function UserorderDetail(headers, orderId) {
   let response = await axios.get(
-    `http://ec2-43-206-254-199.ap-northeast-1.compute.amazonaws.com/api/v1/orders/${orderId}/`,
+    `${BASE_URL}api/v1/orders/${orderId}/`,
     {headers: headers},
   );
   return response.data;
 }
 
 export async function OrderCancel(id,headers){
-  let finalURL = `http://ec2-43-206-254-199.ap-northeast-1.compute.amazonaws.com/api/v1/orders/` + `${id}/canceled_order/`
+  let finalURL = `${BASE_URL}api/v1/orders/` + `${id}/canceled_order/`
   const resp = await axios.post(finalURL,{}, { headers: headers })
   return resp.data
 }
