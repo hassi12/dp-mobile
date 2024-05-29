dffbf3547ee60fa8d86d26ad00bf3e9b3f165a7b
Zaheer Jan <zaheer@cloaked.id>
2024-05-29 13:21:11 +0500

    api change

--- store/authSlice.js
+++ store/authSlice.js
@@ -1,9 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-// import { BASE_URL, LOGIN_ENDPOINT,SIGNUP_ENDPOINT,API_VERSION } from "../utlis/apiUrls";
+import { BASE_URL } from "./../services/base_url";
 
 // const user = JSON.parse(localStorage.getItem('user'))
-export const BASE_URL = process.env.NODE_ENV == 'development' ? `http://ec2-43-206-254-199.ap-northeast-1.compute.amazonaws.com/` : `https://api.cosmedicos.com/`;
-
 const initialState = {
     user: '',
     token: '',
